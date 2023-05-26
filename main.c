64a79673615d1c7ca9f8079883872fafdd1741ed
SHIMO2305 <134682477+SHIMO2305@users.noreply.github.com>
2023-05-26 16:49:22 +0900

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	float a, b;
+	printf("g’·‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢ :");
+	scanf("%f", &a);
+	b = (a - 100)*0.9;
+	printf("•W€‘Ìd‚Í%.1f‚Å‚·", b);
+	return 0;
+}
