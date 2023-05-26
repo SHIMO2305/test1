c10bb7543c736d2e6d81302dc6df49dc19ce31be
MoriK0135 <134683052+MoriK0135@users.noreply.github.com>
2023-05-26 17:39:16 +0900

    Update main.c
    コメントを変えてみました。

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 int main(void)
 {
 	float a, b;
-	printf("身長を入力してくださいね :");
+	printf("身長を入力してくださいね :"); // 身長の入力です！
 	scanf("%f", &a);
 	b = (a - 100)*0.9;
-	printf("標準体重は%.1fです", b);
+	printf("標準体重は%.1fです", b); //標準体重の表示です！
 	return 0;
 }
